53c0477bb5d3c23addffd2bd797c7b2d11a11fc8
hYxA <zybu@live.ru>
2021-10-26 13:29:59 +0300

    setUpAll

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -2,22 +2,29 @@ package ru.netology;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class CardOrderTest {
     private WebDriver driver;
     ChromeOptions options = new ChromeOptions();
+
     options.addArguments("--disable-dev-shm-usage");
     options.addArguments("--no-sandbox");
     options.addArguments("--headless");
-    driver = new ChromeDriver(options);
+    ChromeDriver driver = new ChromeDriver(options);
 
     WebDriverManager wdm = WebDriverManager.chromedriver().browserInDocker()
             .enableVnc().enableRecording();
 
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+    }
     @BeforeEach
     void setupTest() {
         driver = wdm.create();
