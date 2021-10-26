50f02835882dcbb233054e9a5d04eda4da9d7bd0
hYxA <zybu@live.ru>
2021-10-26 11:29:34 +0300

    added chromedriver.exe

---
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -0,0 +1,35 @@
+package ru.netology;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class CardOrderTest {
+    private WebDriver driver;
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--disable-dev-shm-usage");
+    options.addArguments("--no-sandbox");
+    options.addArguments("--headless");
+    driver = new ChromeDriver(options);
+
+    WebDriverManager wdm = WebDriverManager.chromedriver().browserInDocker()
+            .enableVnc().enableRecording();
+
+    @BeforeEach
+    void setupTest() {
+        driver = wdm.create();
+    }
+
+    @AfterEach
+    void teardown() {
+        wdm.quit();
+    }
+
+    @Test
+    void test() {
+        // Your test logic here
+    }
+}
